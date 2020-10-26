f5cd09169e8aa212d2da949b6e9e12f78e3ce30f
mmboyce <mathieumimmsboyce@gmail.com>
2020-10-26 12:43:02 -0400

    Fix path to themoviedb requirement.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var theMovieDb = require('../themoviedb');
+var theMovieDb = require('../themoviedb/themoviedb');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
